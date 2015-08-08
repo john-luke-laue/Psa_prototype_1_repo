681be9e1a3449bf5bdb3331f74554613544c782a
jl_test <jl_test@jl_test.com>
2015-08-08 10:45:00 -0700

    intial commit

---
+++ PSA_Prototype_1/PSA_Prototype_1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PSA_Prototype_1.Startup))]
+namespace PSA_Prototype_1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
